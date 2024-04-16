0db28f2c6ec405eb09d7c55804b28fb8e4309ac6
AdamZou <yz725@cornell.edu>
2024-04-17 00:16:59 +0800

    1. Added tests for generating ML models as files in response. 2. Delete the python version requirement of  < 3.12

--- tests/general_test.py
+++ tests/general_test.py
@@ -13,12 +13,16 @@ def test_codebox() -> None:
 
 
 def test_localbox() -> None:
+    # TODO(adamzou): Ideally, we don't need to specify custom packages.
+    # The agent should be able to install the packages needed automatically.
+    # We may need to change the way we install packages in CodeBox from "!pip" to "% pip",
+    # and also to use an LLM model that can handle the installation process of packages better.
     session = CodeInterpreterSession(
-        custom_packages=["openpyxl", "yfinance"], local=True
+        custom_packages=["openpyxl", "yfinance", "scikit-learn"], local=True
     )
     assert run_sync(session), "Failed to run sync CodeInterpreterSession locally"
     session = CodeInterpreterSession(
-        custom_packages=["openpyxl", "yfinance"], local=True
+        custom_packages=["openpyxl", "yfinance", "scikit-learn"], local=True
     )
     assert asyncio.run(
         run_async(session)
@@ -49,6 +53,17 @@ def run_sync(session: CodeInterpreterSession) -> bool:
             ".png"
             in session.generate_response(
                 "Plot the current stock price of Tesla.",
+            )
+            .files[0]
+            .name
+        )
+
+        assert (
+            ".joblib"
+            in session.generate_response(
+                "Train a simple linear regression model on 'TV advertising' and \
+                'Sales' in this csv file and return the model in joblib format.",
+                files=[File.from_path("examples/assets/advertising.csv")],
             )
             .files[0]
             .name
@@ -89,6 +104,19 @@ async def run_async(session: CodeInterpreterSession) -> bool:
             in (
                 await session.agenerate_response(
                     "Plot the current stock price of Tesla.",
+                )
+            )
+            .files[0]
+            .name
+        )
+
+        assert (
+            ".joblib"
+            in (
+                await session.agenerate_response(
+                    "Train a simple linear regression model on 'TV advertising' and \
+                'Sales' in this csv file and return the model in joblib format.",
+                    files=[File.from_path("examples/assets/advertising.csv")],
                 )
             )
             .files[0]
